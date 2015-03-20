3c61c61ba09ac3391e0a126f5e6518187235b420
Kautuk Waghmare <meet2kautuk@gmail.com>
2015-03-20 11:07:43 +0000

    added ng-include

---
+++ controllers/welcome.js
@@ -0,0 +1,6 @@
+AvenueBlogApp.controller("WelcomeController", function($scope) {
+    $scope.helloTo = {};
+    $scope.helloTo.title = "Avenue Blog";
+    $scope.titlesAvailable = ["JavaScript", "Node JS", "Angular JS"];
+    $scope.popularTags = [{tag:"Open Source", rank: 3}, {tag:"JavaScript", rank: 2}, {tag:"JS", rank: 1}, {tag:"Node JS", rank: 4}];
+ });
