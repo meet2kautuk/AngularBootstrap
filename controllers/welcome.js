f4550752befb6ff9e4efb0c0a7ac1df94841b706
Kautuk Waghmare <meet2kautuk@gmail.com>
2015-03-20 11:18:59 +0000

    added

--- controllers/welcome.js
+++ controllers/welcome.js
@@ -1,6 +1,11 @@
-AvenueBlogApp.controller("WelcomeController", function($scope) {
+AvenueBlogApp.controller("WelcomeController", function($scope, $http) {
     $scope.helloTo = {};
     $scope.helloTo.title = "Avenue Blog";
     $scope.titlesAvailable = ["JavaScript", "Node JS", "Angular JS"];
     $scope.popularTags = [{tag:"Open Source", rank: 3}, {tag:"JavaScript", rank: 2}, {tag:"JS", rank: 1}, {tag:"Node JS", rank: 4}];
+    
+    var url = "data/authors.json";
+    $http.get(url).success(function(response){
+        $scope.authors = response;
+    });
  });
